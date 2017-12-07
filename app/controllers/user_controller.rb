0eef5050ce846237cffcf09abb77320914b982b7
PRbsas <paularamirez0316@gmail.com>
2017-12-07 17:00:14 -0500

    Create post route /signup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,4 +7,15 @@ class UserController < ApplicationController
   get '/login' do
     erb :'/users/login'
   end
+
+  post '/signup' do
+    if params.none? { |k, v| v.blank? }
+      user = User.create(username: params[:username], password: params[:password])
+      user.save
+      session[:user_id] = user.id
+      redirect '/notebooks'
+    else
+      redirect '/signup'
+    end 
+  end
 end
