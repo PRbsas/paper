6eded030aa7b80d0d4797626f3c05115e5619cf4
PRbsas <paularamirez0316@gmail.com>
2017-12-16 11:22:21 -0500

    Modify routes to create notes

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -1,10 +1,19 @@
 class NoteController < ApplicationController
 
-  get '/notebooks/notes/new' do
+  get '/notebooks/:id/notes/new' do
     if logged_in?
+      @notebook = Notebook.find_by_id(params[:id])
       erb :'/notes/create_note'
     else
       redirect '/login'
     end
+  end
+
+  post '/notebooks/:id' do
+    @notebook = Notebook.find_by_id(params[:id])
+    @note = Note.new(content: params[:content]) unless params[:content].empty?
+    @note.save
+    @notebook.notes << @note
+    redirect "/notebooks/#{@notebook.id}"
   end
 end
